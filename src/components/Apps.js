121c9bcd2933caa254396f59aa0b935b45dbe0f7
Travis Neufeld <tdizzletrav@gmail.com>
2020-08-25 16:38:57 -0700

    Add app images, update works cursor

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Fade } from 'react-awesome-reveal';
+import HomeImage from '../assets/Home.png';
+import HistoryImage from '../assets/History.png';
+import FoodEntryImage from '../assets/FoodEntry.png';
 
 function Apps() {
   const [currentApp, setApp] = useState('Logger');
@@ -18,6 +21,12 @@ function Apps() {
             <span>
               Meant to be used while you go. Simply record how you're feeling and what you've eaten. Logger will look for patterns and inform you of foods you should avoid.
             </span>
+
+            <div className="logger-picture-container">
+              <img src={HomeImage} width={150} height={330} alt="logger app home page" />
+              <img src={FoodEntryImage} width={150} height={330} alt="logger app food entry page" />
+              <img src={HistoryImage} width={150} height={330} alt="logger app history page" />
+            </div>
           </div>
         }
       </div>
